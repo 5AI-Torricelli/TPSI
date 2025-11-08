9b4d053fa05bac50e81378f481cac6eff082467b
Tommaso Masetta <masettatommaso@gmail.com>
2025-11-08 10:09:05 +0100

    Messi i commenti in socketJava

--- socketJava/socketClient/src/UDPClient.java
+++ socketJava/socketClient/src/UDPClient.java
@@ -1,43 +1,74 @@
-import java.io.*;
 import java.net.*;
+import java.io.*;
 
+/**
+ * Classe che rappresenta un client UDP.
+ * Gestisce la comunicazione tramite socket UDP.
+ */
 public class UDPClient {
+  /**
+   * Socket UDP utilizzato per la comunicazione.
+   */
   private DatagramSocket socket;
 
+  /**
+   * Costruttore della classe che crea un socket UDP.
+   * 
+   * @throws SocketException se non riesce a creare il socket.
+   */
   public UDPClient() throws SocketException {
+    // Creo un socket UDP e lo imposto con un timeout di 1000 millisecond.
     socket = new DatagramSocket();
     socket.setSoTimeout(1000);
   }
 
+  /**
+   * Chiude il socket UDP.
+   */
   public void close_socket() {
+    // Chiudo il socket UDP.
     socket.close();
   }
 
+  /**
+   * Invia una richiesta al server e restituisce la risposta.
+   * 
+   * @param request la richiesta da inviare.
+   * @param host    l'indirizzo IP del server.
+   * @param port    la porta UDP del server.
+   * @return la risposta del server.
+   * @throws UnknownHostException   se l'indirizzo IP non esiste.
+   * @throws IOException            se si verifica un errore di I/O.
+   * @throws SocketTimeoutException se non riesce a ricevere la risposta
+   *                                entro il timeout specificato.
+   */
   public String sendAndReceive(String request, String host, int port)
       throws UnknownHostException, IOException, SocketTimeoutException {
-    byte[] buffer;
-    DatagramPacket datagram;
-    String answer;
+    // Converto la richiesta in un array di byte.
+    byte[] buffer = request.getBytes("UTF-8");
 
-    InetAddress address = InetAddress.getByName(host);
+    // Creo un DatagramPacket con l'array di byte e l'indirizzo IP del server.
+    DatagramPacket datagram = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(host), port);
 
-    if (socket.isClosed()) {
-      throw new IOException();
-    }
-
-    buffer = request.getBytes("UTF-8");
-
-    datagram = new DatagramPacket(buffer, buffer.length, address, port);
+    // Invio il DatagramPacket attraverso il socket UDP.
     socket.send(datagram);
 
+    // Creo un nuovo DatagramPacket per ricevere la risposta.
+    datagram = new DatagramPacket(buffer, buffer.length);
+
+    // Ricevo la risposta tramite il socket UDP.
     socket.receive(datagram);
 
-    if (datagram.getAddress().equals(address) && datagram.getPort() == port) {
-      answer = new String(datagram.getData(), 0, datagram.getLength(), "ISO-8859-1");
+    // Verifico se la risposta proviene dal server giusto.
+    if (datagram.getAddress().equals(InetAddress.getByName(host)) && datagram.getPort() == port) {
+      // Converto la risposta in una stringa.
+      String answer = new String(datagram.getData(), 0, datagram.getLength(), "ISO-8859-1");
+
+      // Restituisco la risposta.
+      return answer;
     } else {
+      // Lancio un'eccezione se la risposta non proviene dal server giusto.
       throw new SocketTimeoutException();
     }
-
-    return answer;
   }
 }
--- socketJava/socketServer/src/UDPecho.java
+++ socketJava/socketServer/src/UDPecho.java
@@ -4,29 +4,37 @@ import java.io.*;
 public class UDPecho extends Thread {
   private DatagramSocket socket;
 
+  // Costruttore per creare un socket UDP per ricevere
+  // pacchetti UDP sulla porta specificata
   public UDPecho(int port) throws SocketException {
     socket = new DatagramSocket(port);
+    // Imposta il timeout per la ricezione di un pacchetto
     socket.setSoTimeout(1000);
   }
 
   public void run() {
     DatagramPacket answer;
 
+    // Buffer per contenere i dati ricevuti
     byte[] buffer = new byte[1024];
 
+    // Creazione di un DatagramPacket per contenere i dati ricevuti
     DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+
+    // Ciclo per ricevere i pacchetti UDP
     while (!Thread.interrupted()) {
       try {
+        // Ricezione di un pacchetto UDP
         socket.receive(request);
+        // Creazione di un DatagramPacket per contenere la risposta
         answer = new DatagramPacket(request.getData(), request.getLength(),
             request.getAddress(), request.getPort());
-        // Se vogliamo stampare quanto ricevuto da un client
-        // System.out.println("Ricevuto: " + new String(answer.getData(), 0,
-        // answer.getLength(), "ISO-8859-1"));
+        // Invio della risposta
         socket.send(answer);
       } catch (IOException e) {
       }
     }
+    // Chiusura del socket
     socket.close();
   }
 }
